cdd259e5e2bad240e3b2e1f7189c6b575173dd13
TAY TS <taytzushieh@gmail.com>
2019-12-09 21:09:04 +0800

    Remove mock data for analytics and cluster page

--- src/pages/TFIDFPage/saga/api.js
+++ src/pages/TFIDFPage/saga/api.js
@@ -2,14 +2,11 @@ import api from '@apis/api';
 import apiConfig from '@apis/apiConfig';
 
 export function fetchTFIDFJobStatus() {
-  return new Promise(resolve =>
-    setTimeout(() => resolve({ data: { job: 'tfidf', status: 'success' } }), 3000),
-  );
-  // return api.get({
-  //   url: apiConfig.analytics.tfitf,
-  //   query: { job: 'tfidf' },
-  //   needAuthenticate: true,
-  // });
+  return api.get({
+    url: apiConfig.analytics.tfitf,
+    query: { job: 'tfidf' },
+    needAuthenticate: true,
+  });
 }
 
 export function fetchTFIDF({ payload }) {
@@ -25,17 +22,11 @@ export function fetchTFIDF({ payload }) {
     query.filter = searchKey;
   }
 
-  const { data } = require('./mock/mockTFIDFData.json');
-
-  return new Promise(resolve =>
-    setTimeout(() => resolve({ data, pageNum, query: { searchValue, searchKey } }), 1000),
-  );
-
-  // return api
-  //   .get({
-  //     url: apiConfig.analytics.tfidf,
-  //     query,
-  //     needAuthenticate: true,
-  //   })
-  //   .then(({ data }) => ({ data, pageNum, query: { searchValue, searchKey } }));
+  return api
+    .get({
+      url: apiConfig.analytics.tfidf,
+      query,
+      needAuthenticate: true,
+    })
+    .then(({ data }) => ({ data, pageNum, query: { searchValue, searchKey } }));
 }
